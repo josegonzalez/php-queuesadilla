5c854a340b6681f2b3a9407c44f0d6661c5e1a50
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-11-28 14:45:45 -0500

    Add tests for Worker and TestWorker class. Closes #22

---
+++ tests/josegonzalez/Queuesadilla/Worker/TestWorkerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace josegonzalez\Queuesadilla;
+
+use josegonzalez\Queuesadilla\Engine\NullEngine;
+use josegonzalez\Queuesadilla\Worker\TestWorker;
+use josegonzalez\Queuesadilla\TestCase;
+use Psr\Log\LoggerInterface;
+
+class TestWorkerTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->Engine = new NullEngine;
+        $this->Worker = new TestWorker($this->Engine);
+    }
+
+    /**
+     * @covers josegonzalez\Queuesadilla\Worker\Base::__construct
+     * @covers josegonzalez\Queuesadilla\Worker\TestWorker::__construct
+     */
+    public function testConstruct()
+    {
+        $Worker = new TestWorker($this->Engine);
+        $this->assertInstanceOf('\josegonzalez\Queuesadilla\Worker\Base', $Worker);
+        $this->assertInstanceOf('\Psr\Log\LoggerInterface', $Worker->logger());
+    }
+
+
+    /**
+     * @covers josegonzalez\Queuesadilla\Worker\Base::work
+     * @covers josegonzalez\Queuesadilla\Worker\TestWorker::work
+     */
+    public function testWork()
+    {
+        $Worker = new TestWorker($this->Engine);
+        $this->assertTrue($Worker->work());
+        $this->assertTrue($Worker->work());
+        $this->assertTrue($Worker->work());
+    }
+}
