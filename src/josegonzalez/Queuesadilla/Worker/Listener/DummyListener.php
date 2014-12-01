1e68ec77651d41ea242255f268c1372418b10868
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-11-30 19:20:15 -0500

    Add a dummy listener

---
+++ src/josegonzalez/Queuesadilla/Worker/Listener/DummyListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace josegonzalez\Queuesadilla\Worker\Listener;
+
+use josegonzalez\Queuesadilla\Event\Event;
+use josegonzalez\Queuesadilla\Event\MultiEventListener;
+use josegonzalez\Queuesadilla\Utility\LoggerTrait;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+class DummyListener extends MultiEventListener
+{
+    use LoggerTrait;
+
+    public function __construct(LoggerInterface $logger = null)
+    {
+        $this->setLogger($logger);
+    }
+
+    public function implementedEvents()
+    {
+        return [
+            'Worker.connectionFailed' => 'perform',
+            'Worker.maxIterations' => 'perform',
+            'Worker.job.seen' => 'perform',
+            'Worker.job.empty' => 'perform',
+            'Worker.job.invalid' => 'perform',
+            'Worker.job.exception' => 'perform',
+            'Worker.job.success' => 'perform',
+            'Worker.job.failure' => 'perform',
+        ];
+    }
+
+    public function perform(Event $event)
+    {
+        $data = $event->data();
+        $job = '';
+        if (!empty($data['job'])) {
+            $job = $data['job'];
+        }
+
+        $this->logger()->info(sprintf("%s: %s\n", $event->name(), json_encode($job)));
+    }
+}
