762f4968e4574cc5976d3f9e76f742e17e8b7702
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-11-26 23:35:15 -0500

    PHPUnit_Framework_TestCase->skipIf() does not exist

--- tests/josegonzalez/Queuesadilla/Engine/RedisEngineTest.php
+++ tests/josegonzalez/Queuesadilla/Engine/RedisEngineTest.php
@@ -13,7 +13,9 @@ class RedisEngineTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $this->skipIf(!class_exists('Redis'), 'Redis extension is not installed or configured properly.');
+        if (!class_exists('Redis')) {
+            $this->markTestSkipped('Redis extension is not installed or configured properly.');
+        }
 
         $this->url = getenv('REDIS_URL');
         $this->config = ['url' => $this->url];
